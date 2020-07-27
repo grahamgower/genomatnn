7d9159a81ab2e1ab8ceafe49db8797fffc49887c
Graham Gower <graham.gower@gmail.com>
2020-07-27 19:06:47 +0200

    create version file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    use_scm_version=True,
+    use_scm_version={"write_to": "genomatnn/_version.py"},
     entry_points={"console_scripts": ["genomatnn=genomatnn.cli:main"]},
     dependency_links=[
         "git+https://github.com/grahamgower/stdpopsim.git@selection#egg=stdpopsim-99",
